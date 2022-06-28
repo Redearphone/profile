67915259f454fae7b2be4d3a9135b33ea3f7e8bb
Redearphone <kentokun114@gmail.com>
2022-06-28 15:04:26 +0900

    profile

---
+++ js/marvel.js
@@ -0,0 +1,12 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 2000,
+	    speed: 800,
+	    dots: true,
+	    arrows: true,
+	    infinite: true,
+	    pauseOnHover: false,
+	    /*----両サイドを表示----*/
+	    centerMode:true,
+	    centerPadding:"30%",
+  });
